a04b4127cd43839d61b2e46b9710cd45f86f69dc
anggi <ortegacanillo76@gmail.com>
2024-06-02 19:31:48 +0800

    ädded branch for hw4 and finished the homework

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-const express = require("express");
-const logger = require("morgan");
-const cors = require("cors");
-const mongoose = require("mongoose");
-
-const contactsRouter = require("./routes/api/contacts");
+import express from "express";
+import logger from "morgan";
+import cors from "cors";
+import { router as contactsRouter } from "./routes/api/contactsRouter.js";
+import { router as usersRouter } from "./routes/api/usersRouter.js";
 
 const app = express();
 
@@ -13,31 +12,21 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-main()
-  .then(() => {
-    console.log("Database connection successful");
-  })
-  .catch((err) => console.log(`Error: ${err.message}`));
-
-async function main() {
-  await mongoose.connect(
-    "mongodb+srv://aynjel76:pBKgi7Iq5tclP3BB@cluster0.sir4r2u.mongodb.net/db-contacts"
-  );
-}
-
-// test api status check
-app.get("/", (req, res) => {
-  res.json({ status: "success", code: 200 });
+// check api status
+app.get("/", (_req, res) => {
+  res.json({ status: "success" });
 });
 
 app.use("/api/contacts", contactsRouter);
+app.use("/api/users", usersRouter);
 
-app.use((req, res) => {
+app.use((_req, res) => {
   res.status(404).json({ message: "Not found" });
 });
 
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message });
+app.use((err, _req, res, _next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
 });
 
-module.exports = app;
+export { app };
